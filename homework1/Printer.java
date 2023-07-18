d5ba196a951ce4ab44aa275739a890ca05cc74b8
Константин <kostay_mc@mail.ru>
2023-07-18 16:21:57 +0300

    hw1 compl

---
+++ homework1/Printer.java
@@ -0,0 +1,46 @@
+package exception.homework1;
+
+import java.util.Arrays;
+
+public class Printer {
+    public static void main(String[] args) {
+       /* Answer ans = new Answer();
+        try {
+            ans.arrayOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Выход за пределы массива");
+        }
+
+        try {
+            ans.divisionByZero();
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль");
+        }
+
+        try {
+            ans.numberFormatException();
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка преобразования строки в число");
+        }
+    }
+
+        */
+
+        int[] a = {};
+        int[] b = {};
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = new int[]{4, 5, 6};
+            b = new int[]{2, 2, 1, 8};
+        } else {
+            a = Arrays.stream(args[0].split(", ")).mapToInt(Integer::parseInt).toArray();
+            b = Arrays.stream(args[1].split(", ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        Answer ans = new Answer();
+        String itresume_res = Arrays.toString(ans.divArrays(a, b));
+        System.out.println(itresume_res);
+    }
+
+}
