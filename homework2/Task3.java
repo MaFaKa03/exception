717ba4ddcb3c15e5bac71f75a94784a3cd8739f7
Константин <kostay_mc@mail.ru>
2023-07-20 18:02:49 +0300

    hw2 compl

--- homework2/Task3.java
+++ homework2/Task3.java
@@ -8,14 +8,20 @@ public class Task3 {
             int a = 90;
             int b = 3;
             System.out.println(a / b);
-            printSum(23, null);
+            printSum(23, 1);
             int[] abc = { 1, 2 };
             abc[3] = 9;
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
+        }catch (ArithmeticException e){
+            System.out.println("Делить на ноль нельзя");
+        } catch (NullPointerException e){
+            System.out.println("Одна из переменных является Null");
+        } catch (Exception e){
+            System.out.println("Что-то пошло не так...");
         }
     }
-    public static void printSum(Integer a, Integer b) throws NullPointerException {
+    public static void printSum(Integer a, Integer b){
         System.out.println(a + b);
     }
 
